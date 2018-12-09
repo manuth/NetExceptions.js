69df47b6f7538849db1acd5ca3660397133f3bbc
Manuel Thalmann <m@nuth.ch>
2018-12-09 19:01:23 +0100

    Remove unused tests

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -1,13 +1,5 @@
-import * as assert from "assert";
-
 suite(
     "NetExceptions.js",
     () =>
     {
-        test(
-            "Example...",
-            () =>
-            {
-                assert.strictEqual(1, 1);
-            });
     });
